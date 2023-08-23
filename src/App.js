799de5f061de1e99b0a0431de9f994c33d0b47f7
kingov.alexey <kingov_alexey@mail.ru>
2023-08-23 21:56:20 +0600

    # lesson 8 - done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import "./scss/app.scss";
+import './scss/app.scss';
 
-import Header from "./components/Header/Header";
-import Home from './pages/Home'
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-
+import Header from './components/Header/Header';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <>
-      <div className="wrapper">
+      <div className='wrapper'>
         <Header />
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />}/>
-              <Route path="/cart" element={<Cart />}/>
-              <Route path="/*" element={<NotFound />}/>
-            </Routes>
-
-
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
 
           {/* <Home /> */}
           {/* <NotFound /> */}
-          </div>
         </div>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-import Categories from "../components/Categories/Categories";
-import Sort from "../components/Sort/Sort";
-import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
+import Categories from '../components/Categories/Categories';
+import Sort from '../components/Sort/Sort';
+import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
 
 const Home = () => {
   const [pizzas, setPizzas] = React.useState([]);
@@ -11,37 +11,36 @@ const Home = () => {
 
   React.useEffect(() => {
     getPizzaAll();
+    window.scrollTo(0, 0);
   }, []);
 
   function getPizzaAll() {
-    fetch("http://localhost:9999/table-pizzas")
-      .then((res) => {
+    fetch('http://localhost:9999/table-pizzas')
+      .then(res => {
         return res.json();
       })
-      .then((json) => {
+      .then(json => {
         setPizzas(json);
         setIsLoading(false);
       })
-      .catch((err) => {
-        console.log("Ошибка:", err);
+      .catch(err => {
+        console.log('Ошибка:', err);
       });
   }
   return (
-    <>
-    <div className="content__top">
-      <Categories />
-      <Sort />
+    <div className='container'>
+      <div className='content__top'>
+        <Categories />
+        <Sort />
+      </div>
+
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : pizzas.map((obj, id) => <PizzaBlock key={id} {...obj} />)}
+      </div>
     </div>
-    
-    
-    <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items"> 
-              {isLoading
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : pizzas.map((obj, id) => <PizzaBlock key={id} {...obj} />)}
-            </div>
-    </>
-    
   );
 };
 
